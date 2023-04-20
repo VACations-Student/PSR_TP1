6dfb549ef4ec1d06d0d5ddcd7d1e8dbf7b5c7889
Vilches <0vacations0@gmail.com>
2023-04-20 09:37:17 -0300

    Bv

--- Domicilio.ts
+++ Domicilio.ts
@@ -1,7 +1,6 @@
 import { Empresa } from "./Empresa"
 
 export class Domicilio {
-    id_domicilio: Number
     direccion: string
     nombre_empresa: string
     barrio: string
@@ -9,8 +8,7 @@ export class Domicilio {
     dueño: string
     ids_cortes: number[]
 
-    constructor(id_domicilio: number, direccion: string, nombre_empresa: string, barrio: string, consumo: number, dueño: string, ids_cortes:number[]){
-        this.id_domicilio = id_domicilio
+    constructor(direccion: string, nombre_empresa: string, barrio: string, consumo: number, dueño: string, ids_cortes:number[]){
         this.direccion = direccion
         this.nombre_empresa = nombre_empresa
         this.barrio = barrio
