003047eb8f7b76264a906199cd5758870657ca62
Vilches <0vacations0@gmail.com>
2023-05-04 16:12:25 -0300

    Controller cambiado

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,3 +1,20 @@
 import { usuarioModel } from "../models/usarioModel";
 import { Usuario } from "../clases/Usuario";
 import * as bcrypt from "bcrypt";
+
+export default{
+    sign_in: (usuario:Usuario) => {
+        return new Promise<any>((resolve,reject) =>{
+            usuarioModel.create({"name": usuario.name,"password":bcrypt.hash(usuario.password,10)}).then((user)=>{
+                resolve (user)
+            })
+        });
+    },
+    log_in: (usuario:Usuario) => {
+        return new Promise<any>((resolve,reject) =>{
+            usuarioModel.find({"nombre": usuario.name, "password": bcrypt.hash(usuario.password,10)}).then((user)=>{
+                resolve (user)
+            })
+        });
+    }
+}
